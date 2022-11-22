a45d3b0ed0d034a8238836423f72c4f7eb3cf196
Michael200344 <michael.rheintgen@gmail.com>
2022-11-22 16:20:12 -0600

    Added Audio Files

--- audio.py
+++ audio.py
@@ -3,8 +3,9 @@ import os
 
 pygame.init()
 
-bg_music = pygame.mixer.music.load(os.path.join("Assets", "Sounds", "music1.mp3"))
+menu_music = pygame.mixer.music.load(os.path.join("Assets", "Sounds", "menu_music.mp3"))
 jump_land_sound = pygame.mixer.Sound(os.path.join("Assets", "Sounds", "jumpland.wav"))
 jump_sound = pygame.mixer.Sound(os.path.join("Assets", "Sounds", "jump.wav"))
 sword_whoosh_sound = pygame.mixer.Sound(os.path.join("Assets", "Sounds", "sword_whoosh.mp3"))
 running_sound = pygame.mixer.Sound(os.path.join("Assets", "Sounds", "run.wav"))
+death_sound = pygame.mixer.Sound(os.path.join("Assets", "Sounds", "death_sound.mp3"))
